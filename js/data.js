41cb8638cc0ee35b1cb26928fd6fef738766c7fd
sarahcruzz <sarah.cruz0492@gmail.com>
2023-11-06 13:17:04 -0300

    projeto formativo

---
+++ js/data.js
@@ -0,0 +1,8 @@
+const catalogo = [{
+cod_produto: 1,
+nome_produto: "Abafador de Ruído Muffler 3M",
+descricao_produto: "Os Abafadores de Ruído Muffler 3M são designados para proteção do sistema auditivo, ajudando a reduzir a exposição aos níveis perigosos de ruído e outros sons indesejados. Oferece excelente custo benefício com ótima atenuação e com ajuste de altura para melhor conforto do usuário",
+preco_produto: "R$ 89,90",
+imagem_produto: "./img/imagens_produto/epi/abafador.avif",
+}
+]
